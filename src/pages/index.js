7b107330212072a9623a72869fd1dad1ab1d2671
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-10-10 15:25:31 +0500

    fixed

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,9 +38,9 @@ export default function Home() {
 
   if (isLoadingMainPage || isFetchingMainPage) {
     return (
-      <Wrapper>
+      <>
         <ContentLoader />
-      </Wrapper>
+      </>
     );
   }
 
