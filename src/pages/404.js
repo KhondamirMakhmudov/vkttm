e7cdc133103042e58a3ec5f823186c68f4874d77
KhondamirMakhmudov <xondamir.maxmudov.01@mail.ru>
2024-10-11 23:02:04 +0500

    fixed

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,5 @@
 import Wrapper from "@/layout/wrapper";
+import Link from "next/link";
 import React from "react";
 
 const Index = () => {
@@ -7,7 +8,16 @@ const Index = () => {
       <div className="flex items-center text-center justify-center min-h-screen">
         <div>
           <h1 className="text-[120px] font-mulish text-[#00B1BE]">404</h1>
-          <p className="text-[40px]">Siz qidirgan sahifa topilmadi!</p>
+          <p className="text-[40px] mb-[20px]">
+            Siz qidirgan sahifa topilmadi!
+          </p>
+
+          <Link
+            href={"/"}
+            className="border text-[#00B1BE] hover:text-white border-[#00B1BE] bg-white hover:bg-[#00B1BE] px-[35px] py-[10px] rounded-md text-xl"
+          >
+            Bosh sahifa
+          </Link>
         </div>
       </div>
     </>
--- src/pages/500.js
+++ src/pages/500.js
@@ -1,5 +1,6 @@
 import Wrapper from "@/layout/wrapper";
 import React from "react";
+import Link from "next/link";
 
 const Index = () => {
   return (
@@ -10,10 +11,17 @@ const Index = () => {
           <p className="text-[40px] font-medium">
             Kechirasiz, serverda xatolik ):
           </p>
-          <p>
+          <p className="mb-[30px]">
             Serverning ichki xatoligi, iltimos keyinroq qayta urinib
             ko&apos;ring!
           </p>
+
+          <Link
+            href={"/"}
+            className="border text-[#00B1BE] hover:text-white border-[#00B1BE] bg-white hover:bg-[#00B1BE] px-[35px] py-[10px] mt-[30px] rounded-md text-xl"
+          >
+            Bosh sahifa
+          </Link>
         </div>
       </div>
     </>
